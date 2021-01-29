f62c61d08b82e577ccb72c5e5f2febe50b2c2b2f
rutiksangle3436 <rutiksangle3436@gmail.com>
2021-01-29 22:36:48 +0530

    UI Improvements

--- src/Components/Utils/HomeCarousel.js
+++ src/Components/Utils/HomeCarousel.js
@@ -12,24 +12,18 @@ export default function HomeCarousel() {
                 <Carousel.Item>
                     <img className="d-block w-100" src={i1} alt="First slide" height='500' width='50'/>
                     <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="d-block w-100" src={i2} alt="Third slide" height='500' width='200'/>
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img className="d-block w-100" src={i3} alt="Third slide" height='500' width='200' />
 
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
